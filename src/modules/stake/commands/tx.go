eaed9f061b3861ef5b1e7990414c7f17490994a0
Xin Qian <qianxin@dora.network>
2018-09-13 16:43:38 +0800

    Use unified GetPubKey() implementation
    Change-Id: I36b7dd5e8e892456a9d328915fa0a657f0f048d5
    Signed-off-by: Xin Qian <qianxin@dora.network>

--- src/modules/stake/commands/tx.go
+++ src/modules/stake/commands/tx.go
@@ -1,14 +1,11 @@
 package commands
 
 import (
-	"encoding/hex"
 	"fmt"
 
 	"github.com/spf13/cobra"
 	flag "github.com/spf13/pflag"
 	"github.com/spf13/viper"
-
-	"github.com/tendermint/go-crypto"
 
 	txcmd "github.com/dora/ultron/client/commands/txs"
 	"github.com/dora/ultron/modules/stake"
@@ -138,7 +135,7 @@ func init() {
 }
 
 func cmdDeclareCandidacy(cmd *cobra.Command, args []string) error {
-	pk, err := GetPubKey(viper.GetString(FlagPubKey))
+	pk, err := utils.GetPubKey(viper.GetString(FlagPubKey))
 	if err != nil {
 		return err
 	}
@@ -239,25 +236,4 @@ func cmdWithdraw(cmd *cobra.Command, args []string) error {
 
 	tx := stake.NewTxWithdraw(validatorAddress, amount)
 	return txcmd.DoTx(tx)
-}
-
-// GetPubKey - create the pubkey from a pubkey string
-func GetPubKey(pubKeyStr string) (pk crypto.PubKey, err error) {
-	if len(pubKeyStr) == 0 {
-		err = fmt.Errorf("must use --pubkey flag")
-		return
-	}
-	if len(pubKeyStr) != 64 { //if len(pkBytes) != 32 {
-		err = fmt.Errorf("pubkey must be Ed25519 hex encoded string which is 64 characters long")
-		return
-	}
-	var pkBytes []byte
-	pkBytes, err = hex.DecodeString(pubKeyStr)
-	if err != nil {
-		return
-	}
-	var pkEd crypto.PubKeyEd25519
-	copy(pkEd[:], pkBytes[:])
-	pk = pkEd.Wrap()
-	return
 }
